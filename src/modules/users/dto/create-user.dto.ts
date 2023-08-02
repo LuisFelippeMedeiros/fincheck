c4ed58b30839986d976c783779fef7e43535c19a
luisfelippemedeiros <lfelippemedeiros@gmail.com>
2023-08-02 12:09:33 -0400

    Initial and several changes

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'O e-mail não pode ser vazio' })
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'A senha não pode ser vazio' })
+  @MinLength(8)
+  password: string;
+}
